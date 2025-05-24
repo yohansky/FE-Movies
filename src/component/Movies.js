1b156ab0b6570da566ffca965ea6f0a2ef67330f
Yohanes <yohanesdahlan@gmail.com>
2025-05-24 14:11:26 +0700

    check for error fetching

--- src/component/Movies.js
+++ src/component/Movies.js
@@ -2,31 +2,73 @@ import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Movies extends Component {
-  state = { movies: [] };
+  state = {
+    movies: [],
+    isLoaded: false,
+    error: null,
+  };
 
   componentDidMount() {
-    this.setState({
-      movies: [
-        { id: 1, title: 'The Shawshank Redemption', runtime: 142 },
-        { id: 2, title: 'The Godfather', runtime: 175 },
-        { id: 3, title: 'The Dark Knight', runtime: 153 },
-        { id: 4, title: 'Pulp Fiction', runtime: 142 },
-        { id: 5, title: 'Forrest Gump', runtime: 142 },
-      ],
-    });
+    fetch('http://localhost:4000/moviesxx')
+      // .then(res => res.json())
+      .then(res => {
+        console.log('Status Code:', res.status);
+        if (!res.ok) {
+          let err = Error;
+          err.message = `Error: ${res.status} ${res.statusText}`;
+          this.setState({
+            isLoaded: true,
+            error: err,
+          });
+        }
+        return res.json();
+      })
+      .then(json => {
+        this.setState(
+          {
+            movies: json.movies,
+            isLoaded: true,
+          },
+          error => {
+            this.setState({
+              isLoaded: true,
+              error: error,
+            });
+          }
+        );
+      })
+      .catch(err => console.log(err));
   }
+
   render() {
-    return (
-      <Fragment>
-        <h2>Choose a movies</h2>
-        <ul>
-          {this.state.movies.map(m => (
-            <li key={m.id}>
-              <Link to={`/movies/${m.id}`}>{m.title}</Link>
-            </li>
-          ))}
-        </ul>
-      </Fragment>
-    );
+    const { movies, isLoaded, error } = this.state;
+    if (error) {
+      return (
+        <Fragment>
+          <h2>{error.message}</h2>
+        </Fragment>
+      );
+    } else if (!isLoaded) {
+      return (
+        <Fragment>
+          {JSON.stringify(movies)}
+          <h2>Loading...</h2>
+        </Fragment>
+      );
+    } else if (movies.length === 0) {
+      return <h2>No movies found</h2>;
+    } else
+      return (
+        <Fragment>
+          <h2>Choose a movies</h2>
+          <ul>
+            {movies.map(m => (
+              <li key={m.id}>
+                <Link to={`/movies/${m.id}`}>{m.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </Fragment>
+      );
   }
 }
